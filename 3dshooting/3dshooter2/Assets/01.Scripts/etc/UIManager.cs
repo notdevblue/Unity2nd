ace91568d928515f90a9c8cb848c4e864c301c42
HanUseArch <wooyeophan@gmail.com>
2021-09-07 10:38:35 +0900

    old project deleted

--- 3dshooting/3dshooter2/Assets/01.Scripts/etc/UIManager.cs
+++ 3dshooting/3dshooter2/Assets/01.Scripts/etc/UIManager.cs
@@ -14,10 +14,30 @@ public class UIManager : MonoBehaviour
     public Gun playerGun;
     public List<Image> bulletMarkList = new List<Image>();
 
-    private void Start()
+    private void Awake()
     {
+        bulletMarkPannel.GetComponentsInChildren<Image>(bulletMarkList);
+
+        bulletMarkList.RemoveAt(0);
+        bulletMarkList.ForEach(x => x.gameObject.SetActive(false));
+
+        playerGun.UpdateMaxBullet += value => {
+            for(int i = 0; i < value / 10; ++i)
+            {
+                bulletMarkList[i].gameObject.SetActive(true);
+            }
+        };
+
+        playerGun.UpdateBullet += value => {
+            int cnt = (int)Mathf.Floor(value / 10);
+            for(int i = 0; i < bulletMarkList.Count; ++i)
+            {
+                if(!bulletMarkList[i].gameObject.activeSelf) break;
+                bulletMarkList[i].sprite = i < cnt ? markerImages[0] : markerImages[1];
+            }
+        };
         
     }
-
+    
 
 }
